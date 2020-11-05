47951facea98f09978accc65a12ecf495605c486
Diego Ramirez <dramirez@leftraru.nlhpc.cl>
2020-11-05 15:57:30 -0300

    parser fix

--- src/scripts/data_parser.py
+++ src/scripts/data_parser.py
@@ -1,8 +1,6 @@
 import sys
 import glob
 import pandas as pd
-from pathlib import Path
-
 
 args = sys.argv
 conf_type = args[1]
@@ -11,14 +9,16 @@ job_num = args[2]
 source = '../../data/Guacolda/out/*_{0}_*.out'.format(job_num)
 file_list = glob.glob(source)
 
-aux = file_list[0].rsplit('_', maxsplit=2)
-title = '{0}.csv'.format(aux[0])
+
+aux = file_list[0].rsplit('_', maxsplit=2)[0].rsplit('/', maxsplit=1)[1]
+print(aux)
+title = '{0}.csv'.format(aux)
+print(title)
 df_list = [pd.read_table(file) for file in file_list]
 df = pd.concat(df_list)
 
-
-dout = '../../data/{0}'.format(conf_type)
-data_out = Path(dout)
-df.to_csv(data_out / title, encoding='utf-8', index=False)
+dout = '../../data/{0}/{1}'.format(conf_type, title)
+print(dout)
+df.to_csv(dout, encoding='utf-8', index=False)
 
 

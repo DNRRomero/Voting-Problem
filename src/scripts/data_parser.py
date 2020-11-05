e3cb029f5adbe1d248a5db03f7a4b8f024e7ea0c
Diego Ramirez <godierr@ug.uchile.cl>
2020-11-05 14:23:49 -0300

    parser

--- src/scripts/data_parser.py
+++ src/scripts/data_parser.py
@@ -1,17 +1,24 @@
 import sys
 import glob
 import pandas as pd
+from pathlib import Path
+
 
 args = sys.argv
-job_num = (args[1])
+conf_type = args[1]
+job_num = args[2]
 
-title = '*_{0}_*.out'.format(job_num)
-file_list = glob.glob(title)
+source = '../../data/Guacolda/out/*_{0}_*.out'.format(job_num)
+file_list = glob.glob(source)
+
 aux = file_list[0].rsplit('_', maxsplit=2)
 title = '{0}.csv'.format(aux[0])
 df_list = [pd.read_table(file) for file in file_list]
 df = pd.concat(df_list)
 
-df.to_csv(title)
+
+dout = '../../data/{0}'.format(conf_type)
+data_out = Path(dout)
+df.to_csv(data_out / title, encoding='utf-8', index=False)
 
 
--- src/scripts/ring_agreement.py
+++ src/scripts/ring_agreement.py
@@ -10,12 +10,11 @@ c_type = ConfigType.Ring
 size = n
 p_state = 0.5
 seed = 654798203
-steps = 2000
+steps = 3000
 np.random.seed(seed)
 pi = np.random.permutation(n)
 metricList = [Metric.SpinGlass, Metric.Magnetization]
 magnetList = [0]
-samples = 100
 
 agree = {'init_magnet': [], 'length': [], 'Magnetization_mean': [], 'Magnetization_min': [], 'Magnetization_max': [],
          'Magnetization_std': [], 'SpinGlass': [], 'unstables': []}
@@ -24,6 +23,7 @@ conf = createConfig(ConfigType.Ring, n=n)
 for i, mag in enumerate(magnetList):
     init_states = [State.ON] * round((size * (0.5 + mag / 2)) - 10 ** (-9)) + [State.OFF] * round(
         (size * (0.5 - mag / 2) + 10 ** (-9)))
+    np.random.seed()
     np.random.shuffle(init_states)
     init_rule = np.array([Rule.STABLE] * size)
     curr_rule = init_rule

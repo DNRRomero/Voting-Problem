d1b24414911d1b4ba7a0f0d860f31b8dea3ba2e4
Diego Ramirez <godierr@ug.uchile.cl>
2021-01-13 16:56:32 -0300

    parser

--- src/scripts/data_parser.py
+++ src/scripts/data_parser.py
@@ -21,26 +21,29 @@ print('There are {0} files to be merged'.format(len(out_list)))
 aux = out_list[0].rsplit('_', maxsplit=2)[0].rsplit('/', maxsplit=1)[1]
 
 title = '{0}.csv'.format(aux)
+try:
+    a = out_list[0]
+    if args.serial:
+        df_list = []
+        for i, file in enumerate(out_list):
+            njob = file.rsplit('_', maxsplit=1)[1].rsplit('.', maxsplit=1)[0]
+            serial = args.job_num + njob
+            table = pd.read_table(file, sep=',')
+            nrows = len(table.index)
+            table['sample'] = nrows * [serial]
+            df_list.append(table)
+    else:
+        df_list = [pd.read_table(file, sep=',') for file in out_list]
 
-if args.serial:
-    df_list = []
-    for i, file in enumerate(out_list):
-        njob = file.rsplit('_', maxsplit=1)[1].rsplit('.', maxsplit=1)[0]
-        serial = args.job_num + njob
-        table = pd.read_table(file, sep=',')
-        nrows = len(table.index)
-        table['sample'] = nrows * [serial]
-        df_list.append(table)
-else:
-    df_list = [pd.read_table(file, sep=',') for file in out_list]
+    df = pd.concat(df_list)
 
-df = pd.concat(df_list)
+    dout = '../../data/{0}/{1}'.format(args.conf_type, title)
+    print(dout)
+    df.to_csv(dout, encoding='utf-8', index=False)
 
-dout = '../../data/{0}/{1}'.format(args.conf_type, title)
-print(dout)
-df.to_csv(dout, encoding='utf-8', index=False)
-
-for file in out_list:
-    os.remove(file)
-for file in err_list:
-    os.remove(file)
+    for file in out_list:
+        os.remove(file)
+    for file in err_list:
+        os.remove(file)
+except IndexError:
+    print('There are no files to be merged, parsing failed.mill')
